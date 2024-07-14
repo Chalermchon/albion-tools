2528768e3b2a98e6fb8c26b9da1ee70d37b76d4c
Chalermchon Onbua <chalermchon.on@gmail.com>
2024-07-15 00:17:09 +0700

    refactor: format all files

--- svelte.config.js
+++ svelte.config.js
@@ -7,8 +7,6 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
