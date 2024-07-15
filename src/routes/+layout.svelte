62f06468afec43fc08b631103645b19693769042
Chalermchon Onbua <chalermchon.on@gmail.com>
2024-07-16 02:31:35 +0700

    style: set based background and text color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,14 @@
+<!-- Typescript -->
+<script lang="ts">
+</script>
+
+<!-- HTML -->
 <slot />
+
+<!-- CSS -->
+<style lang="postcss">
+	:global(html) {
+		color: theme(colors.zinc.200);
+		background-color: theme(colors.zinc.900);
+	}
+</style>
