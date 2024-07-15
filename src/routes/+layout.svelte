955a13d0bcba520d65d9629dfd654a40907249eb
Chalermchon Onbua <35775095+Chalermchon@users.noreply.github.com>
2024-07-16 03:46:18 +0700

    feat: setup page layout #minor (#1)
    * feat: add navbar component
    
    * feat: add page content component
    
    * style: set based background and text color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
-<script>
-	import '../app.css';
+<!-- Typescript -->
+<script lang="ts">
+	import Navbar from 'components/navbar.svelte';
+	import PageContent from 'components/page-content.svelte';
 </script>
 
-<slot />
+<!-- HTML -->
+<Navbar>Albion Calculator</Navbar>
+<PageContent>
+	<slot />
+</PageContent>
+
+<!-- CSS -->
+<style lang="postcss">
+	:global(html) {
+		color: theme(colors.zinc.200);
+		background-color: theme(colors.zinc.900);
+	}
+</style>
