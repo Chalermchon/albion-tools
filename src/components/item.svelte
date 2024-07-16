c20c8660b4a5284ef3a2a7b5d0c3e88c48c10294
root <root@Chalermchon-PC>
2024-07-17 02:16:22 +0700

    feat(component): add the item component

---
+++ src/components/item.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let name: string;
+	export let imageUrl: string;
+	export let quantity: number | undefined = undefined;
+</script>
+
+<div class="item bg-neutral-700 shadow-xl shadow-neutral-700/50">
+	<img class="scale-125" alt="{name} Icon" src={imageUrl} />
+	<div class="absolute w-7 right-1 bottom-2 text-center font-mono">{quantity ?? ''}</div>
+</div>
