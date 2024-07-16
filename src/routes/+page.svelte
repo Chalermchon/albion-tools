49e34a83bbb90c06df4d95d72df59b819190c20f
root <root@Chalermchon-PC>
2024-07-17 00:07:23 +0700

    style(component): add css class for typography

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,8 @@
 </script>
 
 <AddItemButton />
+
+<h1 class="h1">Heading 1</h1>
+<h2 class="h2">Heading 2</h2>
+<h3 class="h3">Heading 3</h3>
+<p class="p">paragraph</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontSize: {
+				sm: '0.8rem',
+				base: '1rem',
+				xl: '1.728rem',
+				'2xl': '2.488rem',
+				'3xl': '2.986rem'
+			}
+		}
 	},
 	plugins: []
 };
