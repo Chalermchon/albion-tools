0979a681b46b16f267fbd84cb2c9f2a46cb31a74
root <root@Chalermchon-PC>
2024-07-18 03:14:23 +0700

    fix(component): change item to be button and can click

--- src/components/atoms/item.svelte
+++ src/components/atoms/item.svelte
@@ -4,7 +4,7 @@
 	export let quantity: number | undefined = undefined;
 </script>
 
-<div class="item bg-neutral-700 shadow-xl shadow-neutral-700/50">
+<button class="item bg-neutral-700 shadow-xl shadow-neutral-700/50" on:click>
 	<img class="scale-125" alt="{name} Icon" src={imageUrl} />
 	<div class="absolute w-7 right-1 bottom-2 text-center font-mono">{quantity ?? ''}</div>
-</div>
+</button>
