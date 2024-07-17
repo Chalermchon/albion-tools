2f29d3f4fceaea2a9e4bbc5fa8386b2aca910046
root <root@Chalermchon-PC>
2024-07-18 03:21:26 +0700

    feat(component): add the inventory component

---
+++ src/components/organisms/inventory.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import type { ItemsInInventory } from 'lib/albion/types/service.type';
+	import AddItemButton from 'components/atoms/add-item-button.svelte';
+	import Item from 'components/atoms/item.svelte';
+
+	export let items: ItemsInInventory = [];
+
+	const dispatch = createEventDispatcher();
+
+	const handleClickAddButton = () => {
+		dispatch('clickAddItemButton');
+	};
+</script>
+
+<h1 class="h1 mb-4">Inventory</h1>
+<div class="flex flex-row flex-wrap">
+	{#each items as item}
+		<Item imageUrl={item.imageUrl} name={item.name} quantity={item.quantity} />
+	{/each}
+	<AddItemButton on:click={handleClickAddButton} />
+</div>
