92250a9bfccc6f1c99a900f6611d26038e8a42ac
root <root@Chalermchon-PC>
2024-07-20 19:38:43 +0700

    test: add a missing unit test for item catalog service

--- src/lib/albion/services/inventory.service.test.ts
+++ src/lib/albion/services/inventory.service.test.ts
@@ -10,10 +10,10 @@ import {
 	type MockInstance
 } from 'vitest';
 import { Item } from '../entities/item.entity';
-import { ItemRepository } from '../repositories/item.repository';
-import { initialInventoryService } from './inventory.service';
+import { InventoryService } from './inventory.service';
 import { ItemCategory, ResourceItemType } from '../types/entity.type';
 import type { IInventoryService } from '../types/service.type';
+import type { IItemRepository } from '../types/repository.type';
 
 describe('InventoryService', () => {
 	const mockItem = {
@@ -25,22 +25,24 @@ describe('InventoryService', () => {
 		imageUrl: 'https://render.albiononline.com/v1/item/Mock%20Cloth.png'
 	};
 
-	let InventoryService: IInventoryService;
-	let mockItemRepository: {
+	let fakeItemRepository: {
 		findById: MockInstance<[string], Item | null>;
 	};
 
+	let inventoryService: IInventoryService;
+
 	beforeAll(() => {
-		mockItemRepository = {
-			findById: vi.spyOn(ItemRepository.prototype, 'findById')
+		fakeItemRepository = {
+			findById: vi.fn()
 		};
 	});
 	beforeEach(() => {
-		InventoryService = initialInventoryService();
-		mockItemRepository.findById.mockImplementation((id) => {
+		fakeItemRepository.findById.mockImplementation((id) => {
 			if (id === '1') return new Item(mockItem);
 			return null;
 		});
+
+		inventoryService = new InventoryService(fakeItemRepository as unknown as IItemRepository);
 	});
 	afterEach(() => {
 		vi.resetAllMocks();
@@ -51,65 +53,65 @@ describe('InventoryService', () => {
 
 	describe('getItems', () => {
 		it('should return an empty array when no items are added', () => {
-			const items = InventoryService.getItems();
+			const items = inventoryService.getItems();
 			expect(items).toEqual([]);
 		});
 
 		it('should return items with their quantities when items are added', () => {
-			InventoryService.addItem('1', 10);
+			inventoryService.addItem('1', 10);
 
-			const items = InventoryService.getItems();
+			const items = inventoryService.getItems();
 			expect(items).toEqual([{ ...mockItem, quantity: 10 }]);
 		});
 	});
 
 	describe('addItem', () => {
 		it('should add a new item if it does not exist', () => {
-			InventoryService.addItem('1', 5);
+			inventoryService.addItem('1', 5);
 
-			const items = InventoryService.getItems();
+			const items = inventoryService.getItems();
 			expect(items).toContainEqual({ ...mockItem, quantity: 5 });
 		});
 
 		it('should increase the quantity of an existing item', () => {
-			InventoryService.addItem('1', 5);
-			InventoryService.addItem('1', 3);
+			inventoryService.addItem('1', 5);
+			inventoryService.addItem('1', 3);
 
-			const items = InventoryService.getItems();
+			const items = inventoryService.getItems();
 			expect(items).toContainEqual({ ...mockItem, quantity: 8 });
 		});
 
 		it('should not add an item if the item is not found in the repository', () => {
-			InventoryService.addItem('non-existent-id');
+			inventoryService.addItem('non-existent-id');
 
-			const items = InventoryService.getItems();
+			const items = inventoryService.getItems();
 			expect(items).toEqual([]);
 		});
 	});
 
 	describe('removeItem', () => {
 		it('should decrease the quantity of an existing item', () => {
-			InventoryService.addItem('1', 10);
+			inventoryService.addItem('1', 10);
 
-			InventoryService.removeItem('1', 4);
+			inventoryService.removeItem('1', 4);
 
-			const items = InventoryService.getItems();
+			const items = inventoryService.getItems();
 			expect(items).toContainEqual({ ...mockItem, quantity: 6 });
 		});
 
 		it('should remove the item if the quantity to remove is greater than or equal to the quantity in inventory', () => {
-			InventoryService.addItem('1', 10);
+			inventoryService.addItem('1', 10);
 
-			InventoryService.removeItem('1', 10);
+			inventoryService.removeItem('1', 10);
 
-			const items = InventoryService.getItems();
+			const items = inventoryService.getItems();
 			expect(items).toEqual([]);
 		});
 
 		it('should not affect the inventory if the item does not exist', () => {
-			InventoryService.removeItem('non-existent-id', 5);
+			inventoryService.removeItem('non-existent-id', 5);
 
-			const items = InventoryService.getItems();
+			const items = inventoryService.getItems();
 			expect(items).toEqual([]);
 		});
 	});
